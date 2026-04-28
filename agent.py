a7203f3ca9cb55854624a7eb132af660744a3287
Islom <100282951+Islom-Radjapov@users.noreply.github.com>
2026-04-28 22:35:31 +0500

    Update agent.py

--- agent.py
+++ agent.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 ANTHROPIC_API_KEY = os.environ["ANTHROPIC_API_KEY"]
 GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
-GITHUB_USERNAME = os.environ["MY_USERNAME"]
+GITHUB_USERNAME = "Islom-Radjapov"
 
 HEADERS_GITHUB = {
     "Authorization": f"token {GITHUB_TOKEN}",
